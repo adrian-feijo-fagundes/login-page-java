054d808be0280660f63574b43f600cea7a7dd692
adrianfagundes2024 <adrianfagundes2024@gmail.com>
2025-03-05 11:29:40 -0300

    feat: AuthService implemtando login

--- src/main/java/com/mycompany/projetologin/AuthService.java
+++ src/main/java/com/mycompany/projetologin/AuthService.java
@@ -1,4 +1,25 @@
 package com.mycompany.projetologin;
 
 public class AuthService {
+    UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Boolean login(String username, String password) {
+
+        try {
+            UserDTO user = this.userRepository.getUser(username);
+
+            if (PasswordUtils.verifyPassword(password, user.getHashedPassword())) {
+                throw new Exception("Senha incorreta");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao realizar login: " + e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
 }
